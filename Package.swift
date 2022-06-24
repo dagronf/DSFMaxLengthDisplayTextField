144ffd6802e4a7fb13ef8ff704538ecb3edf5e37
Darren Ford <dford_au-reg@yahoo.com>
2022-06-25 07:16:25 +1000

    Fixed demo runtime bug #1
    See https://github.com/dagronf/DSFMaxLengthDisplayTextField/issues/1
    
    Fixed the linkage for VIViewInvalidating to static

--- Package.swift
+++ Package.swift
@@ -12,6 +12,11 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "DSFMaxLengthDisplayTextField",
+			type: .static,
+			targets: ["DSFMaxLengthDisplayTextField"]),
+		.library(
+			name: "DSFMaxLengthDisplayTextFieldDynamic",
+			type: .dynamic,
 			targets: ["DSFMaxLengthDisplayTextField"]),
 	],
 	dependencies: [
@@ -25,7 +30,9 @@ let package = Package(
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "DSFMaxLengthDisplayTextField",
-			dependencies: ["VIViewInvalidating", "DSFAppearanceManager"]),
+			dependencies: [
+				.product(name: "VIViewInvalidatingStatic", package: "VIViewInvalidating"),
+				"DSFAppearanceManager"]),
 		.testTarget(
 			name: "DSFMaxLengthDisplayTextFieldTests",
 			dependencies: ["DSFMaxLengthDisplayTextField"]),
