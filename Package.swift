857a0865cdd6bd3375360abf68c7e91ede42510a
Darren Ford <dford_au-reg@yahoo.com>
2022-08-24 19:52:28 +1000

    Updated to latest appearance manager

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
 		// Dependencies declare other packages that this package depends on.
 		// .package(url: /* package url */, from: "1.0.0"),
 		.package(url: "https://github.com/dagronf/VIViewInvalidating", from: "3.0.0"),
-		.package(url: "https://github.com/dagronf/DSFAppearanceManager", from: "2.0.0"),
+		.package(url: "https://github.com/dagronf/DSFAppearanceManager", from: "3.0.0"),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
